94f0a4eb0227b49f8c934090dc850eaf0a6367d8
mustafaHassoon <M+mustafaHassoon@users.noreply.github.com>
2020-10-22 14:59:18 +0200

    correcting spelling error + deleting unused code

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,34 @@
 //wraping the list inside IIFE
 var dailyNews = (function () {
 
-    var populareArtist = [];
+    var popularArtist = [];
     var apiUrl = 'https://api.themoviedb.org/3/person/popular?api_key=ee0326c2a9a0b787ee75f169ae1003ff&language=en-US&page=1';
-    
     //FUNCTION TO EXTRACT OBJECTS FROM THE LIST
-    
     function getAll() {
-    return populareArtist;
+    return popularArtist;
     }
-    
+    //FUNCTION TO add OBJECTS LIST ITEM
     function add(artist) {
-    populareArtist.push(artist);
+    popularArtist.push(artist);
     }
-    
+    //Function to load pokemon list from API
     function loadList() {
-    return fetch(apiUrl).then(function (response) {
-    return response.json();
-    }).then(function (json) {
-    json.results.forEach(function (artist) {
-    var artist = {
-    name: artist.name,
-    knownFor: artist.known_for[0].original_title,
-    artistImage: artist.profile_path
-    
-    };
-    add(artist);
-    console.log(artist);
-    });
-    }).catch(function (e) {
-    console.error(e);
-    })
+      return $.ajax(apiUrl, { dataType: "json" }).then(function(responseJSON) {
+          return responseJSON;
+        }).then(function(json) {
+          json.results.forEach(function(artist){
+            var artist = {
+              name: artist.name,
+              knownFor: artist.known_for[0].original_title,
+              artistImage: artist.profile_path,
+            };
+            add(artist);
+          });
+        })
+        .catch(function(e) {
+          console.error(e);
+        });
     }
-    
     // FUNCTION TO ADD NEW LISTITEM FOR EACH artist OBJECT
     function addListItem(artist) {
     var $artistList = $(".artist-list");
@@ -50,7 +46,6 @@ var dailyNews = (function () {
     showDetails(artist);
     });
     }
-    
     //FUNCTION TO SHOW DETAILS OF THE LIST ITEM
     function showDetails(artist) {
     loadDetails(artist).then(function () {
@@ -68,9 +63,7 @@ var dailyNews = (function () {
     console.error(e);
     });
     }
-    
     // SHOW MODAL FUNCTION
-    
     var $modalContainer = $('#modal-container');
     function showModal(artist) {
     // Clear existing modal content
@@ -79,58 +72,31 @@ var dailyNews = (function () {
     $modalContainer.addClass('is-visible');
     // Creating div element in DOM
     var modal = $('<div class="modal"></div>');
-    // adding class to div DOM element
-    //$modal.addClass('modal');
     // create closing button in modal content
     var closeButtonElement = $(
       '<button class="modal-close">' + "Close" + "</button>"
     );
     closeButtonElement.on("click", hideModal);
-    //var closeButtonElement = document.createElement('button');
-    //closeButtonElement.classList.add('modal-close');
-    //closeButtonElement.innerText = 'Close';
-    // Add event listener to close modal when botton is clicked
-    //closeButtonElement.addEventListener('click', hideModal);
-
     // Create element for title in modal content
     var modalTitle = $('<h1 class="modal-title">' + artist.name + "</h1>");
-    //var titleElement = document.createElement('h1');
-    //titleElement.innerText = artist.name;
-
     // Create element for release date in modal content
     var knownFor = $(
       '<p class="modal-details">' + "Known for : " + artist.knownFor + "</p>"
     );
-    //var knownFor = document.createElement('p');
-    //knownFor.innerText = 'Known for : ' + artist.knownFor;
-
     // Create img in modal content
-
     var imageElement = $('<img class="modal-img">');
     imageElement.attr("src", 'https://image.tmdb.org/t/p/original' +  artist.artistImage);
-    //var imageElement = document.createElement('img');
-    //imageElement.classList.add('modal-img');
-    //imageElement.setAttribute('src','https://image.tmdb.org/t/p/original' +  artist.artistImage);
-
     // close if the user press esc
     modal.append(closeButtonElement);
     modal.append(imageElement);
     modal.append(modalTitle);
     modal.append(knownFor);
     $modalContainer.append(modal);
-
    };
    function hideModal() {
     var $modalContainer = $("#modal-container");
     $modalContainer.removeClass("is-visible");
   }
-   //function hideModal() {
-    //modalContainer.classList.remove('is-visible');
-    //}
-    
-  
- 
-   
     return {
     add: add,
     getAll: getAll,
@@ -141,26 +107,13 @@ var dailyNews = (function () {
     showModal: showModal,
     hideModal: hideModal,
     };
-    
     })()
     //End of IIFE
-    
     dailyNews.loadList().then(function () {
     // Now the data is loaded!
     dailyNews.getAll().forEach(function (artist) {
     dailyNews.addListItem(artist);
     });
     });
-
-
-    //oroKBRq_-acaYAC4IrrrQYi-fwaZrA-HtmtDf6x9cRPBW1FD
-
-    //d120a29b6fc2414a8b1c0a882ae38c37
-
-    //https://newsapi.org/v2/everything?q=COVID&from=2020-03-16&sortBy=publishedAt&apiKey=d120a29b6fc2414a8b1c0a882ae38c37&pageSize=100&page=1
-
-
-
-    //oroKBRq_-acaYAC4IrrrQYi-fwaZrA-HtmtDf6x9cRPBW1FD
 
    
