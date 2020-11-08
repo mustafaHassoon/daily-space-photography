aa9ca6389a92f03c6cba1fd020836cd342ef96d4
mustafaHassoon <M+mustafaHassoon@users.noreply.github.com>
2020-11-08 20:05:25 +0100

    using bootstrab in the app

--- js/scripts.js
+++ js/scripts.js
@@ -34,16 +34,26 @@ var dailyNews = (function () {
     function addListItem(artist) {
     var $artistList = $(".artist-list");
     var $listItem = $('<li ></li>');
-    var $button = $('<button class="list-class">' + artist.name + "</button>" );
-    $('listItem').append($button);
-    $button.css("background-image", "url('https://image.tmdb.org/t/p/original" + artist.artistImage + "')");
-    $button.css("background-position", "center");
-    $button.css("background-size", "160px 240px");
-    $listItem.append($button);
+
+    var card = $(
+      '<div class="card mt-5" style="width: 18rem; margin:13px;"></div>'
+      );
+    var image = $('<img class="card-img-top" alt="...">');
+    var title = $('<h5 class="card-title">' + artist.name + "</h5>");
+    image.attr("src","https://image.tmdb.org/t/p/original"+artist.artistImage+"");
+    let body = $('<div class="card-body" style="text-align: center;"></div>');
+    let button = $(
+      '<button type="button" class="btn" style="background-color: #d88780; color: white" data-toggle="modal" data-target="#exampleModal">See profile</button>'
+    );
+    card.append(image);
+    card.append(body);
+    body.append(title);
+    body.append(button);
+    $listItem.append(card);
     $artistList.append($listItem);
     // ADDING EVENT LISTENER TO THE BUTTON
-    $button.on("click", function (event) {
-    showDetails(artist);
+    button.on("click", function (event) {
+      showDetails(artist);
     });
     }
     //FUNCTION TO SHOW DETAILS OF THE LIST ITEM
@@ -64,44 +74,32 @@ var dailyNews = (function () {
     });
     }
     // SHOW MODAL FUNCTION
-    var $modalContainer = $('#modal-container');
     function showModal(artist) {
-    // Clear existing modal content
-    $modalContainer.empty();
-    //add class to modal
-    $modalContainer.addClass('is-visible');
-    // Creating div element in DOM
-    var modal = $('<div class="modal"></div>');
-    modal.css("background-image", "url('https://image.tmdb.org/t/p/original" + artist.artistImage + "')");
-    modal.css("background-position", "center");
-    modal.css("background-size", "480px 720px");
-    var modalFooter = $('<div class="modal-footer"></div>');
-    // create closing button in modal content
+      
+    var modalHeader = $(".modal-header");
+    modalHeader.empty();
+    var modalTitle = $(".modal-title");
+    modalTitle.empty();
+    var modalBody = $(".modal-body");
+    modalBody.empty();
+    var modalFooter = $(".modal-footer");
+    modalFooter.empty();
+    var artistName = $('<h5 class="modal-title">' + artist.name + "</h5>");
+    var artistImage = $(
+      '<img class="modal-img" alt="..." style="width: 50%; padding: 30px;">'
+    );
+    artistImage.attr("src","https://image.tmdb.org/t/p/original"+artist.artistImage+"");
     var closeButtonElement = $(
-      '<button class="modal-close">' + "Close" + "</button>"
+      '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' + "Close" + "</button>"
     );
-    closeButtonElement.on("click", hideModal);
-    // Create element for title in modal content
-    var modalTitle = $('<h1 class="modal-title">' + artist.name + "</h1>");
-    // Create element for release date in modal content
     var knownFor = $(
-      '<p class="modal-details">' + "Known for : " + artist.knownFor + "</p>"
+      "<p>" + "<strong>Known for : </strong>" + artist.knownFor + "</p>"
     );
-    // Create img in modal content
-    //var imageElement = $('<img class="modal-img">');
-    //imageElement.attr("src", 'https://image.tmdb.org/t/p/original' +  artist.artistImage);
-    // close if the user press esc
-    modal.append(closeButtonElement);
-    //modal.append(imageElement);
-    modal.append(modalTitle);
-    modal.append(knownFor);
-    modal.append(modalFooter);
-    $modalContainer.append(modal);
+    modalHeader.append(artistName);
+    modalHeader.append(closeButtonElement);
+    modalBody.append(artistImage);
+    modalFooter.append(knownFor);
    };
-   function hideModal() {
-    var $modalContainer = $("#modal-container");
-    $modalContainer.removeClass("is-visible");
-  }
     return {
     add: add,
     getAll: getAll,
@@ -110,7 +108,6 @@ var dailyNews = (function () {
     showDetails: showDetails,
     loadDetails: loadDetails,
     showModal: showModal,
-    hideModal: hideModal,
     };
     })()
     //End of IIFE
